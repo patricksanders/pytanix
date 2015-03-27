5fb935d3f21ab69a2fb94a5a30dab29cab364965
patricksanders <mail@patricksanders.net>
2015-03-26 21:10:00 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PytanixWebApi',
+    version='0.1',
+    description='simple client for the Nutanix REST API',
+    author='Patrick Sanders',
+    author_email='mail@patricksanders.net',
+    url='https://github.com/patricksanders/pytanix',
+    install_requires=['requests>=1.0'],
+    license='LICENSE',
+    py_modules=[])
