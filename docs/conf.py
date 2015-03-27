018d55d8610a341a3d8aa8b8cc88ee612ea08bf3
patricksanders <mail@patricksanders.net>
2015-03-26 20:56:14 -0500

    fixed rtd theme breakage for real this time

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     html_theme = 'default'
 else:
